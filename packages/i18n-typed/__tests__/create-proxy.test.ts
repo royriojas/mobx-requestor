575de2ff6325db946c06ce188580c1f4f72f413d
Roy Riojas <royriojas@gmail.com>
2026-05-31 01:53:18 -0700

    FEAT: Migrate all to milajs org

--- packages/i18n-typed/__tests__/create-proxy.test.ts
+++ packages/i18n-typed/__tests__/create-proxy.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect, beforeEach, jest, afterEach } from 'bun:test';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { Mocker } from 'bun-mock-dough';
+import { Mocker } from '@milajs/bun-mock-dough';
 import { createProxyWithFallback } from '../src/create-proxy';
 
 interface Baz {
